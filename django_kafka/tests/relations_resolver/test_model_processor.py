93620987294a747c1d3edfc88e870df86158f932
bodja <bodja.rules@gmail.com>
2025-07-03 22:41:07 +0200

    fix: `ModelMessageProcessor._aget_missing_relation` wrong return value. fix: add forgotten testcases for `ModelMessageProcessor`.

--- django_kafka/tests/relations_resolver/test_model_processor.py
+++ django_kafka/tests/relations_resolver/test_model_processor.py
@@ -2,6 +2,7 @@ from unittest.mock import AsyncMock, Mock, patch
 
 from django.test import TestCase
 
+from django_kafka.exceptions import TopicNotRegisteredError
 from django_kafka.models import WaitingMessage, WaitingMessageQuerySet
 from django_kafka.relations_resolver.processor.model import ModelMessageProcessor
 from django_kafka.tests.utils import AsyncIteratorMock
@@ -95,3 +96,125 @@ class ModelMessageProcessorTestCase(TestCase):
 
         for relation in to_resolve:
             self.assertIn(relation, result)
+
+    async def test__aget_missing_relation(self):
+        existing_relations = [
+            AsyncMock(**{"aexists.return_value": True}),
+            AsyncMock(**{"aexists.return_value": True}),
+            AsyncMock(**{"aexists.return_value": False}),
+            AsyncMock(**{"aexists.return_value": True}),
+            AsyncMock(**{"aexists.return_value": False}),
+        ]
+        topic = Mock(
+            **{
+                "get_relations.return_value": [
+                    *existing_relations,
+                ],
+            },
+        )
+        msg = Mock()
+
+        result = await self.msg_processor._aget_missing_relation(topic, msg)
+
+        self.assertEqual(result, existing_relations[2])
+
+    @patch("django_kafka.models.WaitingMessage.objects")
+    @patch(
+        "django_kafka.relations_resolver.processor.model.ModelMessageProcessor._aget_missing_relation",
+    )
+    @patch("django_kafka.kafka.consumers.topic")
+    @patch("django_kafka.relations_resolver.processor.model.sync_to_async")
+    async def test_aprocess_messages_topic_not_registered(
+        self,
+        mock_sync_to_async,
+        mock_consumers_topic,
+        mock__aget_missing_relation,
+        mock_qs,
+    ):
+        relation = Mock()
+        model_msg = AsyncMock()
+
+        mock_sync_to_async.return_value = AsyncMock(
+            return_value=AsyncIteratorMock([model_msg]),
+        )
+        mock_consumers_topic.side_effect = TopicNotRegisteredError
+
+        await self.msg_processor.aprocess_messages(relation)
+
+        mock_sync_to_async.assert_called_once_with(mock_qs.for_relation)
+        model_msg.adelete.assert_called_once_with()
+        mock__aget_missing_relation.assert_not_called()
+
+    @patch("django_kafka.models.WaitingMessage.objects")
+    @patch(
+        "django_kafka.relations_resolver.processor.model.ModelMessageProcessor._aget_missing_relation",
+    )
+    @patch("django_kafka.kafka.relations_resolver.await_for_relation")
+    @patch("django_kafka.kafka.consumers.topic")
+    @patch("django_kafka.relations_resolver.processor.model.Message")
+    @patch("django_kafka.relations_resolver.processor.model.sync_to_async")
+    async def test_aprocess_messages_some_relation_is_missing(
+        self,
+        mock_sync_to_async,
+        mock_kafka_message,
+        mock_consumers_topic,
+        mock_await_for_relation,
+        mock__aget_missing_relation,
+        mock_qs,
+    ):
+        relation = Mock()
+        model_msg = AsyncMock()
+        missing_relation = Mock()
+
+        mock_sync_to_async.return_value = AsyncMock(
+            return_value=AsyncIteratorMock([model_msg]),
+        )
+        mock__aget_missing_relation.return_value = missing_relation
+
+        await self.msg_processor.aprocess_messages(relation)
+
+        mock_sync_to_async.assert_called_once_with(mock_qs.for_relation)
+        mock__aget_missing_relation.assert_called_once_with(
+            mock_consumers_topic(),
+            mock_kafka_message(),
+        )
+        mock_await_for_relation.assert_called_once_with(
+            mock_kafka_message(),
+            missing_relation,
+        )
+        model_msg.adelete.assert_called_once_with()
+
+    @patch(
+        "django_kafka.relations_resolver.processor.model.ModelMessageProcessor._aget_missing_relation",
+    )
+    @patch("django_kafka.kafka.relations_resolver.await_for_relation")
+    @patch("django_kafka.kafka.consumers.topic")
+    @patch("django_kafka.relations_resolver.processor.model.Message")
+    @patch("django_kafka.relations_resolver.processor.model.sync_to_async")
+    async def test_aprocess_messages_consume(
+        self,
+        mock_sync_to_async,
+        mock_kafka_message,
+        mock_consumers_topic,
+        mock_await_for_relation,
+        mock__aget_missing_relation,
+    ):
+        relation = Mock()
+        model_msg = AsyncMock()
+
+        sync_to_async_results = [
+            # for model.objects.for_relation() call
+            AsyncMock(return_value=AsyncIteratorMock([model_msg])),
+            # for topic.consume() call
+            AsyncMock(),
+        ]
+
+        mock_sync_to_async.side_effect = sync_to_async_results
+        mock__aget_missing_relation.return_value = None
+
+        await self.msg_processor.aprocess_messages(relation)
+
+        mock_await_for_relation.assert_not_called()
+        mock_sync_to_async.assert_any_call(mock_consumers_topic().consume)
+        sync_to_async_results[1].assert_any_call(mock_kafka_message())
+        model_msg.adelete.assert_called_once_with()
